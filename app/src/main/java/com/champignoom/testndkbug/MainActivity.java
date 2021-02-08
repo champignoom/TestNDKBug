c75f931c0bf8cc493bc2a9c25ed2fc94bb31e379
champignoom <champignoom@gmail.com>
2021-02-08 16:18:15 +0100

    fix broken activity, although not important

--- app/src/main/java/com/champignoom/testndkbug/MainActivity.java
+++ app/src/main/java/com/champignoom/testndkbug/MainActivity.java
@@ -16,11 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NDKWrapper.run_native();
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public static native void wtf();
 }
